1ad25dce743d38a7c6317e9de7154e614f2f29c5
Burhan Mubarok <burhanmubarok@gmail.com>
2020-07-26 04:46:44 +0700

    Init homepage with a simple project status content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="content">
+      <a href="https://github.com/users/burubur/projects/1" target="blank" className="label">Project Status</a>
+    </div>
+  )
 }
