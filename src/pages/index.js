d539529cc1943e6612720281f8e6bd4ab3679bfd
Burhan Mubarok <burhanmubarok@gmail.com>
2020-07-29 04:44:39 +0700

    add navbar menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,23 @@ import React from "react"
 export default function Home() {
   return (
     <div className="content">
-      <a href="https://github.com/users/burubur/projects/1" target="blank" className="label">Project Status</a>
+      <div className="navbar">
+        <span className="brand">Burhan Mubarok</span>
+        <div className="navbar-menu">
+          <span className="navbar-item">Blog</span>
+          <span className="navbar-item">Video</span>
+          <span className="navbar-item">Contact</span>
+        </div>
+      </div>
+      <div className="content-body">
+        <a
+          href="https://github.com/users/burubur/projects/1"
+          target="blank"
+          className="label"
+        >
+          Project Status
+        </a>
+      </div>
     </div>
   )
 }
