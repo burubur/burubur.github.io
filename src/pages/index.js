b6e6dc6c05175e9dfb95dc1110f412d927056965
Burhan Mubarok <burhanmubarok@gmail.com>
2020-07-29 22:41:33 +0700

    add some social link

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,16 @@ export default function Home() {
   return (
     <div className="content">
       <div className="navbar">
-        <span className="brand">Burhan Mubarok</span>
+        <span className="brand">
+          <a href="/">Burhan Mubarok</a>
+        </span>
         <div className="navbar-menu">
-          <span className="navbar-item">Blog</span>
-          <span className="navbar-item">Video</span>
-          <span className="navbar-item">Contact</span>
+          <span className="navbar-item">
+            <a href="https://medium.com/@burubur">Blog</a>
+          </span>
+          <span className="navbar-item">
+            <a href="https://www.youtube.com/c/BurhanMubarok">Video</a>
+          </span>
         </div>
       </div>
       <div className="content-body">
@@ -19,6 +24,23 @@ export default function Home() {
         >
           Project Status
         </a>
+      </div>
+      <div className="footer">
+        <span class="footer-item">
+          <a target="blank" href="https://medium.com/@burubur">
+            Medium
+          </a>
+        </span>
+        <span class="footer-item">
+          <a target="blank" href="https://twitter.com/burhanmubarok">
+            Twitter
+          </a>
+        </span>
+        <span class="footer-item">
+          <a target="blank" href="https://github.com/burubur">
+            Github
+          </a>
+        </span>
       </div>
     </div>
   )
