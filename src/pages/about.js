a83a40253c28310b4fd1e194057a09e323bab640
Burhan Mubarok <burhanmubarok@gmail.com>
2020-08-16 21:47:28 +0700

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./../components/navbar"
+import Footer from "./../components/footer"
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Navbar />
+      <div className="body">
+        burubur.com
+      </div>
+      <Footer />
+    </div>
+  )
+}
