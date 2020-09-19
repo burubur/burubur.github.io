a9ed9b8ce5adc012a40b6bae00df25ee35354269
Burhan Mubarok <burhanmubarok@gmail.com>
2020-09-19 17:26:27 +0700

    add blog index

---
+++ src/pages/blog.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Navbar from "./../components/navbar"
+import Footer from "./../components/footer"
+
+export default function Blog({ data }) {
+  const posts = data.allMarkdownRemark.nodes
+  return (
+    <div className="container">
+      <Navbar />
+      <div className="body">
+        <div className="posts">
+          {posts.map(post => (
+            <div key={post.frontmatter.title} className="post">
+              <Link to={post.frontmatter.slug}>
+                <span className="post-title">{post.frontmatter.title}</span>
+                <br />
+                <span className="post-date">{post.frontmatter.date}</span>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          date
+          slug
+          title
+        }
+      }
+    }
+  }
+`
