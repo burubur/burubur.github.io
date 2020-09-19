67336ab8d237979b85ba3c99fab370f1b149e68f
Burhan Mubarok <burhanmubarok@gmail.com>
2020-09-19 17:25:13 +0700

    change link to a for external page source

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "./../components/layout"
+
 
 export default function Home() {
   return (
     <Layout>
-      <Link
-        to="https://github.com/users/burubur/projects/1"
+      <a
+        href="https://github.com/users/burubur/projects/1"
         target="blank"
         className="label"
       >
         Project Status
-      </Link>
+      </a>
     </Layout>
   )
 }
