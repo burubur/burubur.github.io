970481911321a1df2a3223186c84d6bd30b26e49
Burhan Mubarok <burhanmubarok@gmail.com>
2020-09-20 07:22:20 +0700

    add some helmet for SEO

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,21 @@
 import React from "react"
+import Helmet from "react-helmet"
 import Navbar from "./navbar"
 import Footer from "./footer"
 
 export default function Layout({ children }) {
   return (
-    <div className="container">
-      <Navbar />
-      <div className="body">{children}</div>
-      <Footer />
+    <div className="application">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>burubur.com</title>
+        <link rel="canonical" href="http://burubur.com" />
+      </Helmet>
+      <div className="container">
+        <Navbar />
+        <div className="body">{children}</div>
+        <Footer />
+      </div>
     </div>
   )
 }
