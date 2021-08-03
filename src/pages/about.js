0e6c517c6376d92e7a1082790401103abe563c62
Burhan Mubarok <burhanmubarok@gmail.com>
2021-08-04 00:31:30 +0700

    add photo on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,29 @@
 import React from "react"
 import Layout from "./../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
-  return <Layout>burubur.com</Layout>
+  return (
+    <Layout>
+      <About />
+    </Layout>
+  )
+}
+
+function About() {
+  return (
+    <div className="body">
+      <StaticImage
+        src="../images/photo.jpg"
+        alt="photo"
+        className="photo-profile"
+      />
+      <span className="spacer" />
+      <div className="center">
+        Ordinary Indonesian
+        <br />
+        helping <a href="https://www.gojek.com/">Gojek</a> Engineering.
+      </div>
+    </div>
+  )
 }
