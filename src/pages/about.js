67e5261cf1d64e53de20d310a2071c17898d20c9
Burhan Mubarok <burhanmubarok@gmail.com>
2021-08-04 08:54:37 +0700

    refactor

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,28 +2,22 @@ import React from "react"
 import Layout from "./../components/layout"
 import { StaticImage } from "gatsby-plugin-image"
 
-export default function Home() {
+export default function About() {
   return (
     <Layout>
-      <About />
-    </Layout>
-  )
-}
-
-function About() {
-  return (
-    <div className="body">
-      <StaticImage
-        src="../images/photo.jpg"
-        alt="photo"
-        className="photo-profile"
-      />
-      <span className="spacer" />
-      <div className="center">
-        Ordinary Indonesian
-        <br />
-        helping <a href="https://www.gojek.com/">Gojek</a> Engineering.
+      <div className="body">
+        <StaticImage
+          src="../images/photo.jpg"
+          alt="photo"
+          className="photo-profile"
+        />
+        <span className="spacer" />
+        <div className="center">
+          Ordinary Indonesian
+          <br />
+          helping <a href="https://www.gojek.com/">Gojek</a> Engineering.
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
