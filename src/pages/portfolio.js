889a86d66d00a4f9beaa471f95c1c41642bfad76
Burhan Mubarok <burhanmubarok@gmail.com>
2021-08-10 01:03:24 +0700

    add flex box on portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -6,7 +6,22 @@ export default function About() {
     <Layout>
       <div className="body">
         <div class="typewriter">
-          <span class="center typewriter-text">Portfolio will be here.</span>
+          <span class="center typewriter-text">In progress ...</span>
+        </div>
+        <span class="spacer" />
+        <div className="portfolio">
+          <div className="item">one</div>
+          <div className="item">two</div>
+          <div className="item">three</div>
+          <div className="item">four</div>
+          <div className="item">five</div>
+          <div className="item">six</div>
+          <div className="item">seven</div>
+          <div className="item">eight</div>
+          <div className="item">nine</div>
+          <div className="item">ten</div>
+          <div className="item">eleven</div>
+          <div className="item">twelve</div>
         </div>
       </div>
     </Layout>
